4d3f583ee594f7d4c8e009a5e34679f47367da7f
Adam Constantine <a.e.constantine@gmail.com>
2020-07-29 10:39:13 -0400

    Manual Update

---
+++ SurveyQuestions/Template.sql
@@ -0,0 +1,8 @@
+/*This line always needs to be here at the top*/
+PRAGMA foreign_keys=ON;
+
+
+/*First survey question line needs to indicate the survey number from surveys table, and initial question number (usually surveyNumber X 10000)*/
+INSERT INTO questions (surveyQuestion, questionID, surveyID, question, choices, cdsNO) VALUES (" ", , , " "," "," ");
+
+INSERT INTO questions (surveyQuestion, surveyID, question, choices, cdsNO) VALUES (" ", , " "," "," ");
