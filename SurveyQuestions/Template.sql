0a1440614533b4c96750933916550635d54ddf64
Adam Constantine <a.e.constantine@gmail.com>
2020-09-01 02:19:00 -0400

    CICU Added

--- SurveyQuestions/Template.sql
+++ SurveyQuestions/Template.sql
@@ -5,4 +5,5 @@ PRAGMA foreign_keys=ON;
 /*First survey question line needs to indicate the survey number from surveys table, and initial question number (usually surveyNumber X 10000)*/
 INSERT INTO questions (surveyQuestion, questionID, surveyID, question, choices, cdsNO) VALUES (" ", , , " "," "," ");
 
+/*The second line and all after do not need the questionID field*/
 INSERT INTO questions (surveyQuestion, surveyID, question, choices, cdsNO) VALUES (" ", , " "," "," ");
